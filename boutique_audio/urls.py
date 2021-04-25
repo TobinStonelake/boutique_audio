1a2b8ccccd6b441d266fa4f657057f2dba359986
TobinStonelake <tobinstonelake@outlook.com>
2021-04-25 12:18:02 +0000

    Added home app and templates

--- boutique_audio/urls.py
+++ boutique_audio/urls.py
@@ -1,5 +1,4 @@
 """boutique_audio URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -18,5 +17,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls'))
+    path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')),
 ]
